4720c6f127957a785054da30616055529ee5ce6f
Wescley Matos <wescleymatos@gmail.com>
2017-07-19 00:56:27 -0300

    commit inicial

---
+++ 1 - exemplo/olaMundo.js
@@ -0,0 +1,11 @@
+var OlaMundo = (function () {
+    function OlaMundo() {
+    }
+    OlaMundo.prototype.digaOla = function () {
+        console.log('Ola Mundo!');
+    };
+    return OlaMundo;
+}());
+var ola = new OlaMundo();
+ola.digaOla();
+//# sourceMappingURL=olaMundo.js.map
---
+++ 1 - exemplo/olaMundo.ts
@@ -0,0 +1,8 @@
+class OlaMundo {
+    digaOla() {
+        console.log('Ola Mundo!');
+    }
+}
+
+let ola = new OlaMundo();
+ola.digaOla();
